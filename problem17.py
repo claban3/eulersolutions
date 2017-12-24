f1499df8b8e5bfa3e1ccad8aca219edc0df935ac
corlaban@umich.edu <corlaban@umich.edu>
2017-12-23 23:16:47 -0500

    finished problem 17

--- problem17.py
+++ problem17.py
@@ -1,18 +1,51 @@
+import math
+
 def solve():
-	words = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
-		"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
-                "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred"]
+	words = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+		"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+                "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred", "thousand"]
+
+	count = 0
+	size = 0;
+	for hundred in range(0, 10):
+		while (count < 20):
+			if (hundred == 0):
+				print words[count]
+				size += len(words[count])
+
+			
+			if(hundred > 0):
+				hundredsNum = str(words[hundred] + words[28] + words[count])
+				
+				if((not (count % 10 == 0)) or (count == 10)):
+					hundredsNum = str(words[hundred] + words[28] + "and" + words[count])	
+				size += len(hundredsNum)	
+				print hundredsNum
+
+			count += 1
+
+		tensCount = count
+
+		while (count < 100):
 		
-	
-	count = 1
-	size = 0
-	while(count<1000):
-		if(count <= 20):
-			size += len(words[count])
-		if(count <= 100):
-			size += len(words[(count - 20)//10 + 20]) + len(words[count%10])
-		else:
-			size += len(words[count//100]) + len(words[((count-((count//100)*100)) - 20)//10 + 20 ]) + len(words[count%10]) + 3
-	print size + 11
+			if(hundred == 0):
+				num = str(words[tensCount]+words[count%10])
+				print num
+				size += len(num)
+
+			if(hundred > 0):
+				hundredsNum = str(words[hundred] + words[28] + "and"+words[tensCount]+words[count%10])
+				size += len(hundredsNum)
+				print hundredsNum
+
+			count += 1
+			if(count%10 == 0 and count > 20):
+				tensCount += 1
+
+		count = 0	
+	thousand = words[1] + words[29]
+	print thousand
+	size += len(thousand)
+	print size
 
 solve()
